4c3bf4029b84e8863984b58badb1255e4177f7a1
Rustam @Second_Fry Gubaydullin <secondfry@gmail.com>
2019-10-10 15:19:48 +0300

    day02 ex02

--- day02/ex02/magnifying_glass.php
+++ day02/ex02/magnifying_glass.php
@@ -9,15 +9,45 @@ if (!file_exists($argv[1])) {
   return;
 }
 
-$ret = '';
+function enlarge_title_1($match) {
+  return 'title="' . strtoupper($match[1]) . '"';
+}
+
+function enlarge_title_2($match) {
+  return 'title=' . strtoupper($match[1]);
+}
+
+function enlarge_non_tag($match) {
+  return strtoupper($match[1]) . $match[2] . strtoupper($match[3]);
+}
+
 $data = file_get_contents($argv[1]);
-$start = strpos($data, '<a ');
-$ret .= strsub($data, 0, $start);
-$data = strsub($data, $start);
-$end = strpos($data, '>');
+$data = preg_replace_callback('/(<a.*?>)(.*?)(<\/a>)/', function ($match) {
 
-$editable = strsub($data);
+  $match_title_1 = '/title="([^"]+)"/';
+  $match_title_2 = '/title=([^" ]+)/';
+  $match_non_tag = '/(.*?)(<[^>]+>)(.*?)/';
 
-$regexp_title_esclosed = '/<a.*?(?:title="([^"]+)").*?<\/a>/';
-$regexp_title_plain = '/<a.*?(?:title=([^" ]+)).*?<\/a>/';
-$regexp_inside_link
+  $link_tag_1 = $match[1];
+  $link_tag_1 = preg_replace_callback($match_title_1, 'enlarge_title_1', $link_tag_1);
+  $link_tag_1 = preg_replace_callback($match_title_2, 'enlarge_title_2', $link_tag_1);
+
+  $contents = $match[2];
+  $has_tags = preg_match($match_non_tag, $contents);
+  if ($has_tags) {
+    $contents = preg_replace_callback($match_non_tag, 'enlarge_non_tag', $contents);
+    $contents = preg_replace_callback($match_title_1, 'enlarge_title_1', $contents);
+    $contents = preg_replace_callback($match_title_2, 'enlarge_title_2', $contents);
+  } else {
+    $contents = strtoupper($contents);
+  }
+
+  $link_tag_2 = $match[3];
+
+  $ret = $link_tag_1 . $contents . $link_tag_2;
+
+  return $ret;
+
+}, $data);
+
+echo $data;
