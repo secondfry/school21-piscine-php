41abcfdaae9f403a8d15bf970e685a1d4bd64d44
Rustam @Second_Fry Gubaydullin <secondfry@gmail.com>
2019-10-11 20:18:04 +0300

    day04 ex03

---
+++ day04/ex03/login.php
@@ -0,0 +1,18 @@
+<?php
+
+include_once 'auth.php';
+
+session_start();
+
+$user = $_GET['login'];
+$pass = $_GET['passwd'];
+
+if (!auth($user, $pass)) {
+  $_SESSION['loggued_on_user'] = '';
+  echo 'ERROR' . "\n";
+  return;
+} else {
+  $_SESSION['loggued_on_user'] = $user;
+  echo 'OK' . "\n";
+  return;
+}
