15c2a65139efecac647e65a596c831fae48074e9
Rustam @Second_Fry Gubaydullin <secondfry@gmail.com>
2019-10-11 20:06:00 +0300

    day04 ex02

---
+++ day04/ex02/modif.php
@@ -0,0 +1,44 @@
+<?php
+
+$user = $_POST['login'];
+$pass_old = $_POST['oldpw'];
+$pass_new = $_POST['newpw'];
+$salt = $_POST['submit'];
+
+if (!$user || !$pass_old || !$pass_new || !$salt || $salt !== 'OK') {
+  echo 'ERROR' . "\n";
+  return;
+}
+
+$pass_old = hash('sha512', $pass_old);
+$pass_new = hash('sha512', $pass_new);
+
+$path = '../private';
+$file = $path . '/passwd';
+
+if (!file_exists($file)) {
+  echo 'ERROR' . "\n";
+}
+
+$data = file_get_contents($file);
+$userdata = unserialize($data);
+
+if (!$userdata[$user]) {
+  echo 'ERROR' . "\n";
+  return;
+}
+
+if ($userdata[$user]['passwd'] !== $pass_old) {
+  echo 'ERROR' . "\n";
+  return;
+}
+
+$userdata[$user] = [
+  'login' => $user,
+  'passwd' => $pass_new
+];
+
+$data = serialize($userdata);
+file_put_contents($file, $data);
+
+echo 'OK' . "\n";
