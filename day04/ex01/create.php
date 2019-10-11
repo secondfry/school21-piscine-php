840fbb4e976a8764645df9d08a09912d73650171
Rustam @Second_Fry Gubaydullin <secondfry@gmail.com>
2019-10-11 19:58:02 +0300

    day04 ex01

---
+++ day04/ex01/create.php
@@ -0,0 +1,41 @@
+<?php
+
+$user = $_POST['login'];
+$pass = $_POST['passwd'];
+$salt = $_POST['submit'];
+
+if (!$user || !$pass || !$salt || $salt !== 'OK') {
+  echo 'ERROR' . "\n";
+  return;
+}
+
+$pass = hash('sha512', $pass);
+
+$path = '../private';
+$file = $path . '/passwd';
+
+if (!file_exists($path)) {
+  mkdir($path);
+}
+
+$userdata = [];
+
+if (file_exists($file)) {
+  $data = file_get_contents($file);
+  $userdata = unserialize($data);
+}
+
+if ($userdata[$user]) {
+  echo 'ERROR' . "\n";
+  return;
+}
+
+$userdata[$user] = [
+  'login' => $user,
+  'passwd' => $pass
+];
+
+$data = serialize($userdata);
+file_put_contents($file, $data);
+
+echo 'OK' . "\n";
