f0bf4a41f951e97659682570df1473ed7e7cd606
Rustam @Second_Fry Gubaydullin <secondfry@gmail.com>
2019-10-13 23:47:13 +0300

    Favourites

--- rush00/pages/index.php
+++ rush00/pages/index.php
@@ -8,6 +8,18 @@ require_once 'pieces/header.php';
   Добро пожаловать в наше уютненькое!<br>
   Только у нас Вы можете приобрети лучшие книги по программированию во всем Новом Эдене!
 </p>
+<h2 class="page_title_small">Избранные товары</h2>
+<div class="shop_items">
+<?php
+
+$res = mysqli_query($DB, 'SELECT * FROM `items` LIMIT 2');
+if ($res) {
+  while ($item = mysqli_fetch_assoc($res)) {
+    display_item($DB, $item);
+  }
+}
+?>
+</div>
 <?php
 
 require_once 'pieces/footer.php';
