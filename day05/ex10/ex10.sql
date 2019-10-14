be20a6a149b1fc2bddc05fe0f2f9011d92715daa
Rustam @Second_Fry Gubaydullin <secondfry@gmail.com>
2019-10-14 19:21:56 +0300

    day05 ex10

---
+++ day05/ex10/ex10.sql
@@ -0,0 +1,7 @@
+SELECT
+  `film`.`title` AS 'Title',
+  `film`.`summary` as 'Summary',
+  `film`.`prod_year`
+FROM `film`
+INNER JOIN `genre` ON `genre`.`id_genre` = `film`.`id_genre`
+WHERE `genre`.`name` = 'erotic';
