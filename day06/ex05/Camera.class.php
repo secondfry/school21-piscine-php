af58501e2ca88ea1abe1252bc546fdf94117ac6a
Rustam @Second_Fry Gubaydullin <secondfry@gmail.com>
2019-10-17 00:20:29 +0300

    day06 ex05 - WIP

---
+++ day06/ex05/Camera.class.php
@@ -0,0 +1,195 @@
+<?php
+
+require_once 'Vertex.class.php';
+require_once 'Matrix.class.php';
+
+class Camera {
+
+  public static bool $verbose = false;
+
+  public static function doc(): string {
+    return file_get_contents(get_class() . '.doc.txt');
+  }
+
+  private Vertex $_origin;
+  private Matrix $_orientation;
+  private float $_width = 0;
+  private float $_height = 0;
+  private float $_ratio = 0;
+  private float $_fov = 0;
+  private float $_near = 0;
+  private float $_far = 0;
+
+  private Matrix $_tT;
+  private Matrix $_tR;
+  private Matrix $_view;
+  private Matrix $_projection;
+
+  public function __construct(array $input) {
+    $keys = array_keys($input);
+
+    $this->_origin = $input['origin'];
+    $this->_orientation = $input['orientation'];
+
+    if (in_array('ratio', $keys)) {
+      $this->_ratio = $input['ratio'];
+    } else {
+      $this->_width = $input['width'];
+      $this->_height = $input['height'];
+    }
+
+    $this->_fov = $input['fov'];
+    $this->_near = $input['near'];
+    $this->_far = $input['far'];
+
+    $this->_checkWidthHeight();
+    $this->_checkRatio();
+    $this->_initMatrixes();
+
+    $this->_verboseConstruct();
+    return $this;
+  }
+
+  public function __destruct() {
+    $this->_verboseDestruct();
+  }
+
+  public function _checkWidthHeight() {
+    if ($this->_width && $this->_height) {
+      return;
+    }
+
+    $top = tan($this->_fov / 2) * $this->_near;
+    $bottom = -$top;
+    $right = $top * $this->_ratio;
+    $left = -$right;
+
+    $this->_width = $right - $left;
+    $this->_height = $top - $bottom;
+  }
+
+  public function _checkRatio() {
+    if ($this->_ratio) {
+      return;
+    }
+
+    $this->_ratio = $this->_width / $this->_height;
+  }
+
+  private function _initMatrixes() {
+    $this->_initTranslation();
+    $this->_initRotation();
+    $this->_initView();
+    $this->_initProjection();
+  }
+
+  private function _initTranslation() {
+    $v = new Vector(['dest' => $this->_origin]);
+    $this->_tT = new Matrix([
+      'preset' => Matrix::TRANSLATION,
+      'vtc' => $v->opposite()
+    ]);
+  }
+
+  private function _initRotation() {
+    $this->_tR = $this->_orientation->mirror();
+  }
+
+  private function _initView() {
+    $this->_view = $this->_tR->mult($this->_tT);
+  }
+
+  private function _initProjection() {
+    $this->_projection = new Matrix([
+      'preset' => Matrix::PROJECTION,
+      'fov' => $this->_fov,
+      'ratio' => $this->_ratio,
+      'near' => $this->_near,
+      'far' => $this->_far
+    ]);
+  }
+
+  public function watchVertex(Vertex $worldVertex): Vertex {
+    echo $worldVertex . "\n";
+    $camVertex = $this->_view->transformVertex($worldVertex);
+    // echo $camVertex . "\n";
+    $screenVertex = $this->_projection->transformVertex($camVertex);
+    // echo $screenVertex . "\n";
+    $NDCVertex = new Vertex([
+      'x' => $screenVertex->getX() * -1 / $screenVertex->getZ(),
+      'y' => $screenVertex->getY() * -1 / $screenVertex->getZ()
+    ]);
+    // echo $NDCVertex . "\n";
+
+    // $NDCVertex = new Vertex([
+    //   'x' => ($screenVertex->getX() + $this->_width / 2) / $this->_width,
+    //   'y' => ($screenVertex->getY() + $this->_height / 2) / $this->_height,
+    // ]);
+    // echo $NDCVertex . "\n";
+    // $NDCVertex = $this->_projection->transformVertex($camVertex);
+    // echo $NDCVertex . "\n";
+    // $NDCVertex = $this->_view->transformVertex($this->_projection->transformVertex($worldVertex));
+    // $NDCVertex = $this->_projection->transformVertex($this->_view->transformVertex($worldVertex));
+    // $rasterVertex = clone $NDCVertex;
+    // $ret->setX(($ret->getX() + 1) * $this->_width / 2);
+    // $ret->setY(($ret->getY() - 1) * -1 * $this->_height / 2);
+    
+    $rasterVertex = new Vertex([
+      'x' => ($NDCVertex->getX() - 1) * -1 * $this->_width / 2,
+      'y' => ($NDCVertex->getY() - 1) * -1 * $this->_height / 2,
+    ]);
+    
+    echo $rasterVertex . "\n\n";
+    // echo $ret . "\n\n";
+    // $ret = $rasterVertex;
+    return $rasterVertex;
+  }
+
+  public function watchTriangle(Triangle $worldTriangle): Triangle {
+    return new Triangle(
+      $this->watchVertex($worldTriangle->getA()),
+      $this->watchVertex($worldTriangle->getB()),
+      $this->watchVertex($worldTriangle->getC())
+    );
+  }
+
+  public function watchMesh(array $triangles): array {
+    $ret = [];
+    foreach ($triangles as $triangle) {
+      $ret[] = $this->watchTriangle($triangle);
+    }
+    return $ret;
+  }
+
+  private function _verboseConstruct(): void {
+    if (!Camera::$verbose) {
+      return;
+    }
+
+    echo 'Camera instance constructed' . "\n";
+  }
+
+  private function _verboseDestruct(): void {
+    if (!Camera::$verbose) {
+      return;
+    }
+
+    echo 'Camera instance destructed' . "\n";
+  }
+
+  public function __toString(): string {
+    $ret = 'Camera( ' . "\n";
+    $ret .= '+ Origine: ' . $this->_origin->__toString() . "\n";
+    $ret .= '+ tT:' . "\n";
+    $ret .= $this->_tT->__toString() . "\n";
+    $ret .= '+ tR:' . "\n";
+    $ret .= $this->_tR->__toString() . "\n";
+    $ret .= '+ tR->mult( tT ):' . "\n";
+    $ret .= $this->_view->__toString() . "\n";
+    $ret .= '+ Proj:' . "\n";
+    $ret .= $this->_projection->__toString() . "\n";
+    $ret .= ')';
+    return $ret;
+  }
+
+}
---
+++ day06/ex05/Vertex.class.php
@@ -0,0 +1,121 @@
+<?php
+
+require_once 'Color.class.php';
+
+class Vertex {
+
+  static bool $verbose = false;
+
+  static function doc(): string {
+    return file_get_contents(get_class() . '.doc.txt');
+  }
+
+  private float $_x = 0;
+  private float $_y = 0;
+  private float $_z = 0;
+  private float $_w = 1.0;
+  private Color $_color;
+
+  public function __construct(array $input) {
+    $keys = array_keys($input);
+
+    $this->setX(floatval($input['x']));
+    $this->setY(floatval($input['y']));
+    $this->setZ(floatval($input['z']));
+
+    if (in_array('w', $keys)) {
+      $this->setW(floatval($input['w']));
+    } else {
+      $this->setW(1.0);
+    }
+
+    if (in_array('color', $keys)) {
+      $this->setColor($input['color']);
+    } else {
+      $color = new Color(['red' => 255, 'green' => 255, 'blue' => 255]);
+      $this->setColor($color);
+    }
+    
+    $this->_verboseConstruct();
+    return $this;
+  }
+
+  public function __destruct() {
+    $this->_verboseDestruct();
+  }
+
+  public function getX(): float {
+    return $this->_x;
+  }
+
+  public function setX(float $val): Vertex {
+    $this->_x = $val;
+    return $this;
+  }
+
+  public function getY(): float {
+    return $this->_y;
+  }
+
+  public function setY(float $val): Vertex {
+    $this->_y = $val;
+    return $this;
+  }
+
+  public function getZ(): float {
+    return $this->_z;
+  }
+
+  public function setZ(float $val): Vertex {
+    $this->_z = $val;
+    return $this;
+  }
+
+  public function getW(): float {
+    return $this->_w;
+  }
+
+  public function setW(float $val): Vertex {
+    $this->_w = $val;
+    return $this;
+  }
+
+  public function getColor(): Color {
+    return $this->_color;
+  }
+
+  public function setColor(Color $val): Vertex {
+    $this->_color = $val;
+    return $this;
+  }
+
+  private function _verboseConstruct(): void {
+    if (Vertex::$verbose) {
+      echo $this->__toString() . ' constructed' . "\n";
+    }
+  }
+
+  private function _verboseDestruct(): void {
+    if (Vertex::$verbose) {
+      echo $this->__toString() . ' destructed' . "\n";
+    }
+  }
+
+  public function __toString(): string {
+    $ret = sprintf(
+      'Vertex( x: %.02f, y: %.02f, z:%.02f, w:%.02f',
+      $this->_x,
+      $this->_y,
+      $this->_z,
+      $this->_w
+    );
+
+    if (Vertex::$verbose) {
+      $ret .= ', ' . $this->_color->__toString();
+    }
+
+    $ret .= ' )';
+    return $ret;
+  }
+
+}
